5e759c7065e3479b213fb8466c15d90917ca1378
robin <robintsunny@gmail.com>
2018-06-06 15:18:08 -0700

    Complete Front-End Authentication

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
 
   const store = configureStore();
-  window.store = store;
-  // we don't put the store directly on the window because
-  // it can be confusing when debugging, sometimes giving you access to state
-  // when you shouldn't
   window.getState = store.getState;
-  window.dispatch = store.dispatch; // just for testing!
+  window.dispatch = store.dispatch;
 
-  ReactDOM.render(<h1>Welcome to Preworkout With Strangers</h1>, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
 });
