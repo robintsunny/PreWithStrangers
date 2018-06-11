c246ca9d945d298bd9a3252cc8a5953734b7210e
robin <robintsunny@gmail.com>
2018-06-10 17:08:08 -0700

    Style Pre Times Event Index, incomplete

--- frontend/components/event/event_index.jsx
+++ frontend/components/event/event_index.jsx
@@ -15,78 +15,78 @@ class EventIndex extends React.Component {
     if (!this.props.cities) {
       return <div>loading</div>;
     } else {
-    return (
-      <div >
-        <div className="pre-splash">
+      return (
+        <div>
+          <div className="pre-splash">
+            <div className="pre-sub-splash">
+              <span className="pre-splash-text">Good Training Partners</span>
+              <span className="pre-splash-text-tag">
+                They're Hard To Come By
+              </span>
+            </div>
 
-      <div className="pre-sub-splash">
-          <span className="pre-splash-text">Good Training Partners</span>
-          <span className="pre-splash-text-tag">They're Hard To Come By</span>
-      </div>
-
-          <Link className="create-event-button" to="/new_event">
-            Create a Pre Time!
-          </Link>
-        </div>
-
-        <div className="pre-message-bar">
-          <div className="pre-message-bar-header">
-            Pre With Strangers is pre, with strangers.
+            <Link className="create-event-button" to="/new_event">
+              Create a Pre Time!
+            </Link>
           </div>
 
-          <div className="pre-message-bar-content">
-            For two hours, five-ish athletic strangers sit around to talk.
-            Not about anything in particular.
-            The circumstances are unusual, but that's the point.
-          </div>
-        </div>
+          <div className="pre-message-bar">
+            <div className="pre-message-bar-header">
+              Pre With Strangers is pre, with strangers.
+            </div>
 
-
-        <div className="city-bar">
-          <span className="city-bar-message">Jump To Your City's Pre Times</span>
-
-        <div className="city-bar-items">
-          {this.props.cities.map((city) => {
-            return (
-              <div className="city-bar-item">
-                <a href={`#${city.name}`}>{city.name}</a>
-              </div>
-            )})}
+            <div className="pre-message-bar-content">
+              For two hours, five-ish athletic strangers sit around to talk. Not
+              about anything in particular. The circumstances are unusual, but
+              that's the point.
+            </div>
           </div>
 
+          <div className="city-bar">
+            <span className="city-bar-message">
+              Jump To Your City's Pre Times
+            </span>
 
+            <div className="city-bar-items">
+              {this.props.cities.map(city => {
+                return (
+                  <div className="city-bar-item">
+                    <a href={`#${city.name}`}>{city.name}</a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="cities-events">
+            {this.props.cities.map(city => {
+              return (
+                <React.Fragment>
+                  <span className="city-name">{city.name}</span>
+                  {city.event_ids.map(event_id => {
+                    const event = this.props.events.find(
+                      event => event.id === event_id
+                    );
+                    return (
+                      <span className="city-events">
+                        <Link className="event-name" to={`/events/${event_id}`}>
+                          {event.name}
+                        </Link>
+                        <br />
+                        <span className="event-date">{event.date}</span>
+                        <br />
+                        <span className="event-time">{event.time}</span>
+                      </span>
+                    );
+                  }, this)}
+                </React.Fragment>
+              );
+            }, this)}
+          </div>
         </div>
-
-
-
-          {this.props.cities.map(city => {
-            return (
-              <React.Fragment>
-                <span className="city-name" id={`"${city.name}"`}>
-                  {city.name}
-                </span>
-
-                {city.event_ids.map(event_id => {
-                  const event = this.props.events.find(
-                    event => event.id === event_id
-                  );
-                  return (
-                    <span className="event-index-city-events">
-                      <Link to={`/events/${event_id}`}>{event.name}</Link>
-
-                      {event.date}
-                      {event.time}
-                    </span>
-                  );
-                }, this)}
-              </React.Fragment>
-            );
-          }, this)}
-
-      </div>
-    );
+      );
+    }
   }
-}
 }
 
 export default EventIndex;
